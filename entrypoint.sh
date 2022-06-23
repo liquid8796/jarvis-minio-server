a3908c37b131bbb46e9e06ac413965e1027ccd56
Fat Rat <60702632+liquid8796@users.noreply.github.com>
2022-06-23 09:33:40 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,8 @@ wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
+docker run \
+  -p 9000:9000 \
+  -p 9001:9001 \
+  --name minio \
 ./minio server --address :$PORT /mnt/minio/data
