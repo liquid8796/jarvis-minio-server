b265d19de40321e6026780cb773467ae63c3d25c
Fat Rat <60702632+liquid8796@users.noreply.github.com>
2022-06-23 10:43:10 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@ wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
-./minio server --address :$PORT --console-address ":9001" /mnt/minio/data
+CONSOLE-PORT=9001
+EXPOSE $CONSOLE-PORT
+./minio server --address :$PORT --console-address :$CONSOLE-PORT /mnt/minio/data
